cdb1e3c31299999991d6a81ed069349d5599602f
Sunavya-Amujuri <amujurisunavya22@gmail.com>
2026-03-20 16:01:45 +0530

    Time: 4 ms (99.89%), Space: 45.8 MB (83.91%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean isPalindrome(int x) {
         if(x < 0 || (x%10 == 0 && x!=0)) return false;
            
-        int rev = 0, original = x;
-        while(x != 0){
+        int rev = 0;
+        while(x > rev){
             rev = rev*10 + x%10;
             x /= 10;
         }
-        return original == rev;
+        return rev == x || x == rev/10;
     }
 }
