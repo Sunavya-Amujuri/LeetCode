828c0cebec881f837615f350b09fe0881fe9db21
Sunavya-Amujuri <amujurisunavya22@gmail.com>
2026-03-21 15:47:56 +0530

    Time: 18 ms (99.43%), Space: 80.6 MB (58.1%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -3,14 +3,8 @@ class Solution {
         HashSet<Integer> set = new HashSet<>();
         for(int i=0; i<nums.length; i++)
         {
-            if(set.contains(nums[i]))
-            {
-                return true;
-            }
-            set.add(nums[i]);
-            if(set.size() > k){
-                set.remove(nums[i-k]);
-            }
+            if(i > k) set.remove(nums[i-k-1]);
+            if(!set.add(nums[i])) return true;
         }
         return false;
     }
