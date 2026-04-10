2e082e5dd7c18b94c1db2b3d6c0a8ed1d61da7a5
Sunavya-Amujuri <amujurisunavya22@gmail.com>
2026-04-10 17:58:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (99.08%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,52 +1,5 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if(needle.length() == 0) return 0;
-        char[] text = haystack.toCharArray();
-        char[] pattern = needle.toCharArray();
-        char[] newString = new char[text.length + pattern.length + 1];
-        int i = 0;
-        for(char ch : pattern){
-            newString[i++] = ch;
-        }
-        newString[i++] = '$';
-        for(char ch : text){
-            newString[i++] = ch;
-        }
-        int[] z = calculate(newString);
-        List<Integer> result = new ArrayList<>();
-        for(int j=0; j<z.length; j++){
-            if(z[j] == pattern.length){
-                return j - pattern.length - 1;
-            }
-        }
-        return -1;
-    }
-    public static int[] calculate(char[] input){
-        int[] z = new int[input.length];
-        int l = 0, r = 0;
-        for(int k=1; k<input.length; k++){
-            if(k > r){
-                l = r = k;
-                while(r < input.length && input[r] == input[r-l]){
-                    r++;
-                }
-                z[k] = r-l;
-                r--;
-            } else {
-                int k1 = k-l;
-                if(z[k1] < r-k+1){
-                    z[k] = z[k1];
-                }
-                else {
-                    l = k;
-                    while(r < input.length && input[r] == input[r-l]){
-                        r++;
-                    }
-                    z[k] = r-l;
-                    r--;
-                }
-            }
-        }
-        return z;
+        return haystack.indexOf(needle);
     }
 }
