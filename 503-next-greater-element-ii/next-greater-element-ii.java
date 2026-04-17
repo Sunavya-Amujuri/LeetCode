4adfa8654bd0d90b1d8c3796cf2817d65c5fcba1
Sunavya-Amujuri <amujurisunavya22@gmail.com>
2026-04-17 13:14:11 +0530

    Time: 9 ms (89.80%) | Memory: 48.8 MB (13.97%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -4,18 +4,16 @@ class Solution {
         int[] ans = new int[nums.length];
         Stack<Integer> st = new Stack<>();
 
-        // initialize all with -1
+        // initialize all with -1.
         Arrays.fill(ans, -1);
 
-        for(int i = 0; i < 2 * n; i++){
-            int idx = i % n;
-
+        for(int i=0; i<2*n; i++){
+            int idx = i%n;
             while(!st.isEmpty() && nums[st.peek()] < nums[idx]){
                 ans[st.pop()] = nums[idx];
             }
-
             if(i < n){
-                st.push(idx);
+                st.push(i);
             }
         }
         return ans;
